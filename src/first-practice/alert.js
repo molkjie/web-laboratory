daac67c58dcc840ebdadad02492cf050fe450d64
Kate Chebanenko <ekaterinacebanenko2005@gmail.com>
2025-02-20 15:23:08 +0200

    add first practice

---
+++ src/first-practice/alert.js
@@ -0,0 +1,2 @@
+const MyName = "Kate Chebanenko";
+alert(MyName);
---
+++ src/first-practice/index.js
@@ -0,0 +1,47 @@
+//task 4
+// let user1, user2;
+// user1 = "Kate";
+// user2 = user1;
+// alert(user2);
+
+
+
+//task 5
+// let password = prompt("Введіть пароль:");
+
+// if (password === "111") {
+//     alert("Рівень доступу: A");
+// } else if (password === "222") {
+//     alert("Рівень доступу: Б");
+// } else {
+//     alert("Доступ заборонено!!!");
+// }
+
+//task 6
+
+// let userName = prompt("Введіть ваше ім'я:");
+// userName = userName.toLowerCase();
+// alert(userName === "admin" ? "Привіт, адміністратор" :
+//   "Доступ обмежений! Зверніться до адміністратора системи.");
+
+
+//task 7
+
+// let day = prompt("Введіть номер дня тижня (1-7):");
+
+// switch (day) {
+//     case "1":
+//     case "2":
+//     case "3":
+//     case "4":
+//     case "5":
+//         alert("Робочий день");
+//         break;
+//     case "6":
+//     case "7":
+//         alert("Вихідний");
+//         break;
+   
+//     default:
+//         alert("Некоректне значення! Введіть число від 1 до 7.");
+// }
