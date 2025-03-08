58d6c5a8483b0bb66ac0ee864518e26e890213f7
Kate Chebanenko <ekaterinacebanenko2005@gmail.com>
2025-03-08 15:23:43 +0200

    add third practice

---
+++ src/third-practice/main.js
@@ -0,0 +1,39 @@
+//task 1
+// function compareNumbers(a, b) {
+//   return a < b ? a : b;
+// }
+
+// console.log(compareNumbers(10, 5));
+
+
+//task 2
+// const compareNumbers = function(a, b) {
+//   return a < b ? a : b;
+// }
+
+// console.log(compareNumbers(10, 5));
+
+//task 3
+// const compareNumbers=(a, b) => {
+//   return a < b ? a : b;
+// }
+
+// console.log(compareNumbers(10, 5));
+
+//task 4
+// function createProgression(base, times) {
+//   if (times <= 0) return String(base);
+
+//   let result = [];
+//   for (let i = 1; i <= times; i++) {
+//     result.push(base * i);
+//   }
+
+//   return result.join(", ");
+// }
+
+// console.log(createProgression(7, 3)); 
+// console.log(createProgression(7, 2)); 
+// console.log(createProgression(7, 1)); 
+// console.log(createProgression(7, 0));
+// console.log(createProgression(7, -5)); 
